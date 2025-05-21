8db13e3025bb3d2d9aab7b2aef6cca2830339c97
janeta7 <janeetaamurtie@gmail.com>
2025-05-21 19:46:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("helloo")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let have some fun with this web  [docs.streamlit.io](https://docs.streamlit.io/)."
 )
